5c8948005d78229a4d9205f7d2fa30d87010b30a
tor-ato <toratokitahara@gmail.com>
2024-10-10 18:10:41 +0900

    client maybe done

--- client.c
+++ client.c
@@ -11,12 +11,42 @@
 /* ************************************************************************** */
 
 #include "libft/includes/libft.h"
+#include <complex.h>
+#include <signal.h>
 #include <unistd.h>
 
-#define PID_ERR_NUM -1
-#define PID_ERR_MSG "Error: invaled PID"
+#define PID_ERR_NUM		-1
+#define PID_ERR_MSG		"Error: invaled PID"
+#define CHAR_BIT_LEN	8
 
 #define USAGE_MSG	"Usage: client [server PID] [message]"
+#define ERR_SEND	"Error: faild to send signal"
+
+void	send_message(pid_t pid, const char *message)
+{
+	unsigned int	bit;
+	char			c;
+	int				res;
+
+	while(*message)
+	{
+		c = *message++;
+		bit = CHAR_BIT_LEN;
+		while (bit--)
+		{
+			if (c & (1 << bit))
+				res = kill(pid, SIGUSR2);
+			else
+				res = kill(pid, SIGUSR1);
+			if (res == -1)
+			{
+				ft_putendl_fd(ERR_SEND, STDIN_FILENO);
+				exit(EXIT_FAILURE);
+			}
+			usleep(100);
+		}
+	}
+}
 
 pid_t	parse_pid(const char *pid_str)
 {
@@ -43,6 +73,7 @@ int main(int argc, char *argv[])
 	if (server_pid <= PID_ERR_NUM)
 	{
 		ft_putendl_fd(PID_ERR_MSG, STDERR_FILENO);
+		return (EXIT_FAILURE);
 	}
 	return (EXIT_SUCCESS);
 }
